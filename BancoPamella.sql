ed3682a6f6b7db10dc3afad980f5787a609d8d00
PAMELLA CHRISTINI PERETO E SILVA <pamella.cpesilva@senacsp.edu.br>
2022-05-17 16:15:52 -0300

    Criação do banco de dados

---
+++ BancoPamella.sql
@@ -0,0 +1,13 @@
+/**
+	Agenda de Contatos
+    @author Pamella Pereto
+*/
+-- Verificar banco de dados 
+show databases;
+-- Criar um novo banco da dados
+create database teste;
+create database agendapamella;
+-- Excluir um banco de dados
+drop database teste;
+-- Selecionar um banco de dados
+use agendapamella;
