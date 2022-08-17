328a7080470f009049c8647343f11605cd87ab8f
Pamella Pereto <pamellapereto@gmail.com>
2022-08-17 17:18:15 -0300

    Update dbSenhorittaV1.3.sql

--- dbSenhorittaV1.3/dbSenhorittaV1.3.sql
+++ dbSenhorittaV1.3/dbSenhorittaV1.3.sql
@@ -15,19 +15,26 @@ create table usuarios(
 	idusu int primary key auto_increment,
     usuario varchar(50) not null,
     login varchar(15) not null unique,
-    senha varchar(50) not null,
+    senha varchar(255) not null,
     perfil varchar(10) not null
 );
+
 
 describe usuarios;
 
 -- Para inserir uma senha com criptografia usa-se md5()
 insert into usuarios(usuario,login,senha,perfil)
 values('Administrador','admin',md5('admin'),'admin');
+
 insert into usuarios(usuario,login,senha,perfil)
 values('Pamella Pereto','pamellapereto',md5('123456'),'user');
 
+insert into usuarios (usuario,login,senha,perfil)
+values ('José de Assis', 'josedeassis',md5('654321'),'user');
+
 select * from usuarios;
+
+update usuarios set usuario = 'José de Assis', login = 'root', senha = md5('root'), perfil = 'admin' where idusu = 3;
 
 -- Acessando o sistema pela tela de login
 -- and (função lógica onde todas as condições devem ser verdadeiras)
@@ -56,11 +63,22 @@ obs varchar(255)
 
 describe fornecedores;
 
-insert into  fornecedores (cnpj,razao,fantasia,fone,cep,endereco,numero,bairro,cidade,uf)
-values ('99.152.736/0001-33','Roberta Campos','Estilo Confecções','(11) 92621-6085','03728-000','Rua São José do Campestre','390','Jardim Danfer','São Paulo','SP');
+insert into  fornecedores (cnpj, razao,fantasia,fone,cep,endereco,numero,bairro,cidade,uf)
+values ('99.152.736/0001-33','Roberta Campos','Arazze','(11) 92621-6085','03728-000','Rua São José do Campestre','390','Jardim Danfer','São Paulo','SP');
 
 insert into  fornecedores (cnpj,razao,fantasia,fone,cep,endereco,numero,bairro,cidade,uf)
 values ('22.245.615/0001-23','José de Assis','Altamoda','(11) 97854-1243','03348-000','Rua Miguel Leão','190','Vila Silvia','São Paulo','SP');
+
+insert into  fornecedores (cnpj,razao,fantasia, site, fone, contato, email, cep,endereco,numero,bairro,cidade,uf)
+values ('71.345.649/0001-77','Evandro Ramos','Shein', 'www.shein.com', '(11) 94216-7915', 'Andreza Martins', 'shein@contato.com.br', '04319-100','Rua Martins','4090','Vila Granada','Rio de Janeiro','RJ');
+
+insert into  fornecedores (cnpj, ie, im, razao,fantasia,fone,cep,endereco,numero, complemento, bairro,cidade,uf)
+values ('34.246.213/0001-11', '88448866-88', '12347764-31', 'Joaquim Silva','C&A','(21) 94369-0164','14296-022','Avenida Brasil','942', 'Portaria 4', 'Jardim Oliveiras','Americana','SP');
+
+insert into  fornecedores (cnpj, ie, im, razao,fantasia,fone,cep,endereco,numero,bairro,cidade,uf, obs)
+values ('96.941.346/0001-99', '34797751-42', '07199723-14', 'Adriana Calcanhoto','Renner','(22) 97516-9463','02796-010','Avenida Monteiro Lobato','15','Vila Albertina','Salvador','BA', 'Fornecedor estrangeiro (pagamento em dólar)');
+
+
 
 select * from fornecedores;
 
@@ -84,60 +102,64 @@ create table produtos(
 describe produtos;
     
 insert into produtos (barcode,produto,descricao,fabricante,estoque,estoquemin,unidade,localizacao,custo,lucro,idfor)
-values ('1111111111','blusa','verde','Estilo Confecções',20,5,'UN','Setor Blusas',59.90,65,1);
+values ('1111111111','blusa','verde','Estilo Confecções',20,5,'UN','Setor Blusas',59.90,65/100,1);
 
 insert into produtos (barcode,produto,descricao,fabricante,estoque,estoquemin,unidade,localizacao,custo,lucro,idfor)
-values ('2222222222','vestido','amarelo','Estilo Confecções',14,9,'UN','Setor Vestidos',39.90,50,1);
+values ('2222222222','vestido','amarelo','Estilo Confecções',14,9,'UN','Setor Vestidos',39.90,50/100,1);
 
 insert into produtos (barcode,produto,descricao,fabricante,estoque,estoquemin,unidade,localizacao,custo,lucro,idfor)
-values ('3333333333','cropped','azul','Estilo Confecções',19,13,'UN','Setor Blusas',99.90,100,1);
+values ('3333333333','cropped','azul','Estilo Confecções',19,13,'UN','Setor Blusas',99.90,100/100,1);
 
 insert into produtos (barcode,produto,descricao,fabricante,estoque,estoquemin,unidade,localizacao,custo,lucro,idfor)
-values ('4444444444','calça','branca','Altamoda',7,10,'UN','Setor Calças',33.90,100,2);
+values ('4444444444','calça','branca','Altamoda',7,10,'UN','Setor Calças',33.90,100/100,2);
 
 insert into produtos (barcode,produto,descricao,fabricante,estoque,estoquemin,unidade,localizacao,custo,lucro,idfor)
-values ('5555555555','vestido','listrado','Altamoda',9,12,'UN','Setor Vestidos',49.90,70,2);
+values ('5555555555','vestido','listrado','Altamoda',9,12,'UN','Setor Vestidos',49.90,70/100,2);
     
 select * from produtos;
 
--- Obter o preço de venda de cada produto
-select codigo as código, produto, custo,
-(custo + ((custo * lucro)/100)) as venda
-from produtos;
-
 -- Inventário de estoque (patrimônio)
 -- sum() ➙ função de soma no banco de dados
-select sum(estoque * custo) as total from produtos;
+select sum(estoque * custo) as Total from produtos;
 
 -- Relatório de reposição de estoque 1
 select * from produtos where estoque < estoquemin;
 
 -- Relatório de reposição de estoque 2
-select codigo, produto, fabricante,
-estoque, estoquemin as estoque_mínimo, custo, lucro
+-- date_format() ➙ função usada para formatar a data
+-- %d/%m/%Y dd/mm/aaaa | %d/%m/%y dd/mm/aa
+select codigo,produto,
+date_format(dataval, '%d/%m/%Y') as data_validade,
+estoque, estoquemin as estoque_mínimo
 from produtos where estoque < estoquemin;
 
--- Relatório de estoque (há quanto tempo o produto encontra-se estocado)
+-- Relatório de validade de produtos 1
+select codigo,produto,
+date_format(dataval, '%d/%m/%Y') as data_validade
+from produtos;
+
+-- Relatório de validade de produtos 2
 -- datediff() ➙ calcular a diferença em dias
 -- curdate() ➙ obtém a data atual
 select codigo,produto,
-date_format(datacad, '%d/%m/%Y') as data_cadastro,
-datediff(datacad, curdate()) as diasEmEstoque
+date_format(dataval, '%d/%m/%Y') as data_validade,
+datediff(dataval, curdate()) as dias_restantes
 from produtos;
+
 
 create table clientes (
  idcli int primary key auto_increment,
- nome varchar (100) not null,
- fone varchar(20) not null,
- cpf varchar(14) unique,
-email varchar(50),
-marketing char(1) not null,
-cep varchar(9),
-endereco varchar(60),
-bairro varchar(50),
-numero varchar(10),
-complemento varchar(50),
-cidade varchar(50),
+ nome varchar (255) not null,
+ fone varchar(255) not null,
+ cpf varchar(255) unique,
+email varchar(255),
+marketing varchar(255) not null,
+cep varchar(255),
+endereco varchar(255),
+bairro varchar(255),
+numero varchar(255),
+complemento varchar(255),
+cidade varchar(255),
 uf char(2)
 );
 
@@ -146,43 +168,33 @@ alter table clientes add column nascimento date after cpf;
 describe clientes;
 
 insert into clientes (nome, fone, cpf, nascimento, email, marketing)
-values ('Pamella Pereto', '1196455-6220', '383.036.508-07', 19970714, 'pamellapereto@gmail.com', 's');
+values ('Pamella Pereto', '1196455-6220', '383.036.508-07', 19970714, 'pamellapereto@gmail.com', 'sim');
 
 insert into clientes (nome, fone, cpf, nascimento, email, marketing)
-values ('Henrique Neto', '1196182-4236', '377.314.998-04', 19911004, 'henriquens4@gmail.com', 's');
+values ('Henrique Neto', '1196182-4236', '377.314.998-04', 19911004, 'henriquens4@gmail.com', 'sim');
 
 insert into clientes (nome, fone, email, marketing)
-values ('Pedro Pereira', '2194292-7163', 'pedropereira@gmail.com', 's');
+values ('Pedro Pereira', '2194292-7163', 'pedropereira@gmail.com', 'sim');
 
 insert into clientes (nome, fone, marketing)
-values ('Roberto Macedo', '2191237-3765', 'n');
+values ('Roberto Macedo', '2191237-3765', 'não');
 
-insert into clientes (nome, fone, marketing, cep, endereco, bairro, numero, complemento, cidade, uf)
-values ('Rosangela Aparecida', '1194123-7766', 'n', '03728-000', 'Rua São José do Campestre', 'Jardim Danfer', '390', 'Casa 2', 'São Paulo', 'SP');
+insert into clientes (nome, fone, marketing, cep, endereco, numero, complemento, bairro, cidade, uf)
+values ('Rosangela Aparecida', '1194123-7766', 'não', '03728-000', 'Rua São José do Campestre', '390', 'Casa 2', 'Jardim Danfer', 'São Paulo', 'SP');
 
-insert into clientes (nome, fone, marketing, cep, endereco, bairro, numero, cidade, uf)
-values ('Ana Dutra', '1193695-2312', 'n', '03324-100', 'Rua Miguel Leão', 'Vila Silvia', '150', 'São Paulo', 'SP');
+insert into clientes (nome, fone, marketing, cep, endereco, numero, bairro, cidade, uf)
+values ('Ana Dutra', '1193695-2312', 'não', '03324-100', 'Rua Miguel Leão', '150', 'Vila Silvia', 'São Paulo', 'SP');
 
 select * from clientes;
 
 update clientes set fone = '1197231-5819' where idcli=6;
 
--- Relatório personalizado de clientes 1 (focando em contato)
-select nome, fone as contato, email
-from clientes;
-
--- Relatório personalizado de clientes 2 (focando em aniversário)
-select nome,
-date_format(nascimento, '%d/%m/%Y') as data_nascimento
-from clientes;
-
--- Relatório personalizado de clientes 3 (focando em e-mail marketing)
-select nome, email from clientes where marketing = 's';
-
-
 -- foreign key(FK): chave estrangeira que cria o relacionamento
+-- do tipo 1-N com a tabela clientes
 -- FK(pedidos)______________PK(clientes)
 -- Observação: Usar o mesmo nome e tipo de dados nas chaves (PK e FK)
+
+-----------------------------------------
 
 create table pedidos (
 pedido int primary key auto_increment,
@@ -193,14 +205,10 @@ foreign key (idcli) references clientes(idcli)
 );
 
 -- Abertura de pedidos
-insert into pedidos (idcli) values(3);
-
-insert into pedidos (idcli) values(4);
+insert into pedidos(idcli) values(1);
 
 -- Verificar pedidos
-select * from pedidos where pedido = 1;
-
-select * from pedidos where pedido = 2;
+select * from pedidos where pedido;
 
 -- Verificar pedidos junto com o nome do cliente
 -- inner join (unir informações de 2 ou mais tabelas)
@@ -214,14 +222,14 @@ select
 pedidos.pedido,
 date_format(pedidos.dataped, '%d%m%Y - %H:%i') as data_ped,
 clientes.nome as cliente,
-clientes.fone as contato
+clientes.fone
 from pedidos inner join clientes
 on pedidos.idcli = clientes.idcli;
 
 
--- ====== Linha de tabela de carrinho ======
+-- ====== Linha de tabela de carrinho =======
 -- Tabela de apoio para criar um relacionamento de tipo M-M
--- (Muitos para Muitos), neste caso não cria-se a chave primária
+-- (Muitos para Muitos), neste caso não criamos a chave primária
 
 
 create table carrinho (
@@ -232,29 +240,23 @@ create table carrinho (
     foreign key(codigo) references produtos(codigo)
 );
 
-insert into carrinho values (1,1,3);
-insert into carrinho values (1,2,1);
-insert into carrinho values (1,3,2);
-
-insert into carrinho values (2,1,2);
-insert into carrinho values (2,2,1);
-insert into carrinho values (2,3,4);
-
+insert into carrinho values (1,8,3);
+insert into carrinho values (1,9,1);
 
 select * from carrinho;
 
 -- Exibir o carrinho
 select pedidos.pedido,
-carrinho.codigo as código,
+carrinho.codigo as Código,
 produtos.produto,
 carrinho.quantidade,
-(produtos.custo + ((produtos.custo * produtos.lucro)/100)) as venda,
-((produtos.custo + ((produtos.custo * produtos.lucro)/100)) * carrinho.quantidade)  as subtotal
+produtos.venda,
+produtos.venda * carrinho.quantidade as Subtotal
 from (carrinho inner join pedidos on carrinho.pedido = pedidos.pedido)
 inner join produtos on carrinho.codigo = produtos.codigo;
 
 -- Total do pedido (encontrado em carrinho) ➙ Fechamento
-select sum((produtos.custo + ((produtos.custo * produtos.lucro)/100)) * carrinho.quantidade) as total
+select sum(produtos.venda * carrinho.quantidade) as Total
 from carrinho inner join produtos on carrinho.codigo = produtos.codigo;
 
 -- Atualização do estoque
